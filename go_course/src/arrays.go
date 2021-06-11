428be942e684641c594c5501807052ddabbb04f6
Juan Gama <juangamag93@gmail.com>
2021-06-11 16:27:38 -0500

    How to use arrays with Go

---
+++ go_course/src/arrays.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Array
+	var array [4]int
+	array[0] = 1
+	array[1] = 2
+	fmt.Println(array, len(array), cap(array))
+	//slice
+	slice := []int{0, 1, 2, 3, 4, 5, 6}
+	fmt.Println(slice, len(slice), cap(slice))
+
+	//metodos en el slice
+	fmt.Print(slice[0])
+	fmt.Println(slice[0:3])
+	fmt.Println(slice[2:4])
+	fmt.Println(slice[4:])
+
+	//append
+	slice = append(slice, 7)
+	fmt.Println(slice)
+	//append new list
+	new_slice := []int{8, 9, 10}
+	slice = append(slice, new_slice...)
+	fmt.Println(slice)
+}
