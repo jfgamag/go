8f826e5b407eb58ac4f3faaf3d1095bb6373c6a2
Juan Gama <juangamag93@gmail.com>
2021-06-18 12:32:54 -0500

    Added parameters model, motor, weight

--- go_course/src/structs.go
+++ go_course/src/structs.go
@@ -3,12 +3,15 @@ package main
 import "fmt"
 
 type car struct {
-	brand string
-	year  int
+	brand  string
+	model  string
+	motor  string
+	weight string
+	year   int
 }
 
 func main() {
-	myCar := car{brand: "Ford", year: 2020}
+	myCar := car{brand: "Ford", model: "F-150", motor: "4.5 L", weight: "5 ton", year: 2020}
 	fmt.Println(myCar)
 
 	//Alternative
